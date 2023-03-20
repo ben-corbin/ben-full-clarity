f40cde54726f994fb7407822c8749cf144ae7f0a
ben-corbin <93767273+ben-corbin@users.noreply.github.com>
2023-03-20 17:04:59 +0000

    change auth settings

--- src/auth.js
+++ src/auth.js
@@ -5,7 +5,7 @@ const TOKEN_COOKIE_KEY = import.meta.env.VITE_APP_API_AUTH_TOKEN;
 //note API key is not secret and can be seen in the browser
 
 export function setAuthToken(token) {
-  Cookies.set(TOKEN_COOKIE_KEY, token, { secure: true, sameSite: 'strict', httpOnly: true });
+  Cookies.set(TOKEN_COOKIE_KEY, token, { secure: true, sameSite: 'strict', httpOnly: false });
 }
 
 export function getAuthToken() {
