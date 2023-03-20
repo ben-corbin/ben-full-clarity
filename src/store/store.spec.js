fb93cdd114c9c537f4575df70662da29c0a0c6f4
ben-corbin <93767273+ben-corbin@users.noreply.github.com>
2023-03-20 09:16:22 +0000

    add tests to store

--- src/store/store.spec.js
+++ src/store/store.spec.js
@@ -1,1 +1,40 @@
-// TO DO - write tests for store
+import { createTestStore } from '@/store';
+import { fetchContacts } from '@/services/api';
+import { afterEach, beforeEach, describe, expect, it, vi, contains } from 'vitest'
+
+describe('store', () => {
+  let store;
+
+  beforeEach(() => {
+    store = createTestStore();
+  });
+
+  describe('actions', () => {
+    describe('fetchContacts', () => {
+      it('should fetch contacts', async () => {
+        const contacts = await fetchContacts();
+        await store.dispatch('fetchContacts');
+        expect(contacts).toEqual(store.state.contacts);
+      });
+    });
+    describe('create group without data', () => {
+      it('should not create a group', async () => {
+        const response = await store.dispatch('createGroup');
+        expect(response).toEqual(undefined);
+      });
+    });
+    describe('create group with data', () => {
+      it('should create a group', async () => {
+        const response = await store.dispatch('createGroup', {
+          groupName: 'test',
+          selectedContacts: ['test'],
+          groupImage: 'test',
+        });
+        expect(response).toEqual(undefined);
+      });
+    });
+  }); 
+});
+
+// TO DO - add more tests
+
